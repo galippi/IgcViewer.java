e8943a3ed7d6641aeedb6c5d0b3a75a599a9acf1
Gabor Liptak <liptakgabor@freemail.hu>
2016-11-30 20:12:24 +0100

    MapImage, MapPanel and igcImage classes are separated

--- src/igcViewer/NumberAdditionUI.java
+++ src/igcViewer/NumberAdditionUI.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package my.numberaddition;
+package igcViewer;
 
 //import igc.IGC_Altitude;
 //import igc.IGC_Coordinate;
@@ -63,85 +63,6 @@ class IgcFiles
   {
     return (val > SNA_doubleLimit);
   }
-}
-
-class igcImage extends threadImage
-{
-  public igcImage(java.awt.Component parent, java.awt.Graphics g, IgcFiles igcFiles)
-  {
-    super(parent, g);
-    this.igcFiles = igcFiles;
-    ctr = 0;
-  }
-  IgcFiles igcFiles;
-  @Override
-    protected void Drawing()
-  {
-    java.awt.Graphics2D g = img.createGraphics();
-    g.setColor(Color.red);
-    g.fillOval(img.getWidth() / 2, img.getHeight() / 2, img.getWidth() / 2 - 5, img.getHeight() / 2 - 5);
-    g.setColor(Color.white);
-    g.fillRect(40,30,200, 100);
-    g.setColor(Color.green);
-    g.drawString("igcImage.drawString ctr=" + ctr, 40, 40);ctr++;
-    g.dispose();
-  }
-  int ctr;
-}
-
-class MapImage extends threadImage
-{
-  public MapImage(java.awt.Graphics g, IgcFiles igcFiles)
-  {
-    super(g);
-    this.igcFiles = igcFiles;
-  }
-  IgcFiles igcFiles;
-}
-
-class MapPanel extends javax.swing.JPanel
-{
-    IgcFiles igcFiles;
-    public MapPanel(IgcFiles igcFiles)
-    {
-      this.igcFiles = igcFiles;
-      ctr = 0;
-      img = new java.awt.image.BufferedImage(100, 50,
-                 java.awt.image.BufferedImage.TYPE_INT_ARGB);
-      java.awt.Graphics2D g2 = img.createGraphics();
-      g2.setColor(Color.red);
-      g2.fillOval(img.getWidth() / 2, img.getHeight() / 2, img.getWidth() / 2 - 5, img.getHeight() / 2 - 5);
-      g2.dispose();
-      igc = new igcImage(this, img.getGraphics(), null);
-      Timer timer = new Timer();
-      timer.schedule(new TimerTask() {
-        @Override
-          public void run() {
-            //repaint();
-            igc.ready = false;
-          }
-      }, 5000);
-    }
-    @Override
-    public void paintComponent(java.awt.Graphics g) {
-        super.paintComponent(g);
-
-        g.drawString("ctr=" + ctr + " size=" + igcFiles.size(), 20, 20);
-        ctr++;
-        g.drawRect(200, 200, 200, 200);
-        //java.awt.Graphics gc = java.awt.GraphicsConfiguration.createCompatibleImage(600, 400);
-        //java.awt.Image img = new java.awt.Image();
-        g.drawImage(img, 100, 100, null);
-        g.drawImage(igc.img, 200, 200, null);
-        if (ctr % 10 == 0) igc.ready = false;
-    }
-    public void Repaint()
-    {
-      igc.ready = false;
-    }
-    int ctr;
-    java.awt.image.BufferedImage img;
-    igcImage igc;
 }
 
 public class NumberAdditionUI extends javax.swing.JFrame {
---
+++ src/igcViewer/igcImage.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package igcViewer;
+
+import java.awt.Color;
+import utils.threadImage;
+
+/**
+ *
+ * @author liptakok
+ */
+public class igcImage extends threadImage
+{
+  public igcImage(java.awt.Component parent, java.awt.Graphics g, IgcFiles igcFiles)
+  {
+    super(parent, g);
+    this.igcFiles = igcFiles;
+    ctr = 0;
+  }
+  IgcFiles igcFiles;
+  @Override
+    protected void Drawing()
+  {
+    java.awt.Graphics2D g = img.createGraphics();
+    g.setColor(Color.red);
+    g.fillOval(img.getWidth() / 2, img.getHeight() / 2, img.getWidth() / 2 - 5, img.getHeight() / 2 - 5);
+    g.setColor(Color.white);
+    g.fillRect(40,30,200, 100);
+    g.setColor(Color.green);
+    g.drawString("igcImage.drawString ctr=" + ctr, 40, 40);ctr++;
+    g.dispose();
+  }
+  int ctr;
+}
