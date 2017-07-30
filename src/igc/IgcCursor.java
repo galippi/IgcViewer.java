7d1808f4a020cf6cdd30352460dc798d300b97f8
Gabor Liptak <liptakgabor@freemail.hu>
2017-07-30 12:49:03 +0200

    IgcCursor class is created

---
+++ src/igc/IgcCursor.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package igc;
+
+import igc.IgcFiles;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author liptak
+ */
+public class IgcCursor {
+  public IgcCursor()
+  {
+    uiParents = new ArrayList<java.awt.Component>();
+  }
+  public IgcCursor(IgcFiles igcFiles)
+  {
+    call main constructor!!!
+    set(igcFiles);
+  }
+  public int size()
+  {
+    return igcFiles.size();
+  }
+  public void set(IgcFiles igcFiles)
+  {
+    this.igcFiles = igcFiles;
+    if (igcFiles.size() < 1)
+    { // no file is loaded
+      timeCursor = -1;
+    }else
+    if (timeCursor < igcFiles.t_min)
+    { // file is loaded -> set the cursor to the beginning of the file
+      timeCursor = igcFiles.t_min;
+    }else
+    if (timeCursor > igcFiles.t_max)
+    {
+      timeCursor = igcFiles.t_max;
+    }
+  }
+  public IgcFiles igcFiles;
+  public int timeCursor = -1;
+  ArrayList<java.awt.Component> uiParents;
+}
