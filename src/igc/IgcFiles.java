91ec6b1df78fd5b716b9d42085842c8f95cb8184
Gabor Liptak <liptakgabor@freemail.hu>
2018-12-26 15:00:02 +0100

    b288fb0b7b7c9255e72016d6b5e6050db9aa4a5a: File handling is improved to support the file close

--- src/igc/IgcFiles.java
+++ src/igc/IgcFiles.java
@@ -22,46 +22,7 @@ public class IgcFiles
   public void add(igc file)
   {
     igcFiles.add(file);
-    updateLimits();
-  }
-  void updateLimits()
-  {
-    if (igcFiles.size() >= 1)
-    {
-      igc file = igcFiles.get(0);
-      lon_min = file.lon_min.val();
-      lon_max = file.lon_max.val();
-      lat_min = file.lat_min.val();
-      lat_max = file.lat_max.val();
-      t_min = (int)file.t_min.t;
-      t_max = (int)file.t_max.t;
-      alt_min = file.alt_min.val();
-      alt_max = file.alt_max.val();
-      for (int idx = 1; idx < igcFiles.size(); idx++)
-      {
-        file = igcFiles.get(idx);
-        lon_min = Math.min(lon_min, file.lon_min.val());
-        lon_max = Math.max(lon_max, file.lon_max.val());
-        lat_min = Math.min(lat_min, file.lat_min.val());
-        lat_max = Math.max(lat_max, file.lat_max.val());
-        if ((int)file.t_min.t < t_min)
-          t_min = (int)file.t_min.t;
-        if ((int)file.t_max.t > t_max)
-          t_max = (int)file.t_max.t;
-        if (file.alt_min.val() < alt_min)
-          alt_min = file.alt_min.val();
-        if (file.alt_max.val() > alt_max)
-          alt_max = file.alt_max.val();
-      }
-    }else
-    { // no file
-    }
-  }
-  public void setTimeOffset(int idx, int offset)
-  {
-    igc file = igcFiles.get(idx);
-    file.setTimeOffset(offset);
-    updateLimits();
+    updateLimits(size() - 1);
   }
   public int size()
   {
@@ -70,6 +31,26 @@ public class IgcFiles
   public void reinit()
   {
     igcFiles.clear();
+    resetLimits();
+  }
+  public igc get(int idx)
+  {
+    return igcFiles.get(idx);
+  }
+  public void close(int idx)
+  {
+      igcFiles.remove(idx);
+      updateLimits();
+  }
+  void updateLimits()
+  {
+      for (int i = 0; i < size(); i++)
+      {
+          updateLimits(i);
+      }
+  }
+  void resetLimits()
+  {
     t_min = 0;
     t_max = 0;
     lon_min = 0;
@@ -79,9 +60,34 @@ public class IgcFiles
     alt_min = 0;
     alt_max = 0;
   }
-  public igc get(int idx)
+  void updateLimits(int i)
   {
-    return igcFiles.get(idx);
+    igc file = get(i);
+    if (i == 0)
+    {
+      lon_min = file.lon_min.val();
+      lon_max = file.lon_max.val();
+      lat_min = file.lat_min.val();
+      lat_max = file.lat_max.val();
+      t_min = (int)file.t_min.t;
+      t_max = (int)file.t_max.t;
+      alt_min = file.alt_min.val();
+      alt_max = file.alt_max.val();
+    }else
+    {
+      lon_min = Math.min(lon_min, file.lon_min.val());
+      lon_max = Math.max(lon_max, file.lon_max.val());
+      lat_min = Math.min(lat_min, file.lat_min.val());
+      lat_max = Math.max(lat_max, file.lat_max.val());
+      if ((int)file.t_min.t < t_min)
+        t_min = (int)file.t_min.t;
+      if ((int)file.t_max.t > t_max)
+        t_max = (int)file.t_max.t;
+      if (file.alt_min.val() < alt_min)
+        alt_min = file.alt_min.val();
+      if (file.alt_max.val() > alt_max)
+        alt_max = file.alt_max.val();
+    }
   }
   public int t_min, t_max;
   public double lon_min, lon_max;
