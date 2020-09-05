6eb08ed984f6cb317e675ee8f9c1b7e71a2cc9ba
Gabor Liptak <liptakgabor@freemail.hu>
2020-09-05 11:52:50 +0200

    ESC handler is added

--- src/igcViewer/OptionsDialog.java
+++ src/igcViewer/OptionsDialog.java
@@ -12,13 +12,19 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+
 import utils.dbg;
 
 /**
@@ -37,6 +43,7 @@ public class OptionsDialog extends JDialog {
     debugLevel = new JTextField("" + dbg.get(), 5);
     //debugLevel.setSize(100,20);
     debugLevel.setHorizontalAlignment(JTextField.TRAILING);
+    //debugLevel.
 
     JPanel jpDebugLevel = new JPanel();
     //jpDebugLevel.setLayout(new GroupLayout(jpDebugLevel));
@@ -60,7 +67,6 @@ public class OptionsDialog extends JDialog {
     JPanel bOkCancel = new JPanel();
     bOkCancel.add(bOk);
     bOkCancel.add(bCancel);
-
     Container cp = getContentPane();
     // add label, text field and button one after another into a single column
     cp.setLayout(new BoxLayout(cp, BoxLayout.Y_AXIS));
@@ -70,6 +76,15 @@ public class OptionsDialog extends JDialog {
     int pw = parent.getWidth();
     setBounds(pt.x + pw / 2 - 150, pt.y + 200, 400, 400);
     this.setMinimumSize(new Dimension(350, 300));
+    ActionListener escListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            cancelHandler();
+        }
+    };
+    getRootPane().registerKeyboardAction(escListener,
+            KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+            JComponent.WHEN_IN_FOCUSED_WINDOW);
   }
   void okHandler()
   {
@@ -89,5 +104,6 @@ public class OptionsDialog extends JDialog {
   void cancelHandler()
   {
     setVisible(false);
+    //dispose();
   }
 }
