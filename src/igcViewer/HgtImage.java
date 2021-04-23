881920ccd20bfdb39b1c337fb9763bb597d5d96b
Gabor Liptak <liptakgabor@freemail.hu>
2021-04-23 07:55:46 +0200

    MapHeightColor class is added Implementation of HgtImage is continued hgt module is updated

--- src/igcViewer/HgtImage.java
+++ src/igcViewer/HgtImage.java
@@ -2,6 +2,9 @@ package igcViewer;
 
 import java.awt.Color;
 
+import hgt.HgtFile;
+import hgt.HgtFileCache;
+import hgt.HgtFileState;
 import igc.GeoUtil;
 import utils.threadImage;
 
@@ -15,13 +18,37 @@ public class HgtImage  extends threadImage {
       protected void Drawing()
     { /* drawing function */
       java.awt.Graphics2D g = img.createGraphics();
+      double lat = gu.lat_min;
+      double lon = gu.lon_min;
+      while(lat < gu.lat_max)
+      {
+          double latNext = (int)(lat + 0.9999999);
+          while(lon < gu.lon_max)
+          {
+              double lonNext = (int)(lon + 0.9999999);
+              HgtFile hgtFile = hgtFileCache.get(lat, lon, false);
+              if (hgtFile != null)
+              {
+                  hgtFile.loadAsync();
+                  if (hgtFile.getState() == HgtFileState.HgtFileLoaded)
+                  {
+                      int x0 = gu.getPosX(lon);
+                      int y0 = gu.getPosY(lat);
+                      int x1 = gu.getPosX(lonNext);
+                      int y1 = gu.getPosY(latNext);
+                  }
+              }
+              lon = lonNext;
+          }
+          lat = latNext;
+      }
       Color baseColor = Color.gray;
       g.setColor(new Color(baseColor.getRed(), baseColor.getGreen(), baseColor.getBlue(), 127));
       //g.fillOval(img.getWidth() / 2, img.getHeight() / 2, img.getWidth() / 2 - 5, img.getHeight() / 2 - 5);
       g.setColor(Color.BLUE);
       g.drawString("HgtImage!", 40, 300);
       g.dispose();
-  }
+    }
     public void setGeoUtil(GeoUtil gu)
     {
       if (!this.gu.isEqual(gu))
@@ -32,4 +59,5 @@ public class HgtImage  extends threadImage {
       }
     }
     GeoUtil gu;
+    HgtFileCache hgtFileCache = new HgtFileCache(IgcViewerPrefs.getSrtmCache());
 }
